3c9a33926ba50096cf94534fa00ac51710fd81cc
Ian Young <ian.greenleaf@gmail.com>
2011-11-08 00:20:25 -0800

    Test for alternate method naming

--- spec/rspecify/transformers/test_class_and_methods_spec.rb
+++ spec/rspecify/transformers/test_class_and_methods_spec.rb
@@ -63,5 +63,26 @@ describe RSpecify::Transformers::TestClassAndMethods do
       end
     }) }
   end
+
+  describe "class extending ActiveSupport::TestCase with alternate method naming" do
+    subject { %{
+      class MyClass < ActiveSupport::TestCase
+        test "should be valid" do
+          something
+          1
+        end
+      end
+    } }
+
+    it { pending "not yet implemented"
+    should transform_to("describe block", %{
+      describe MyClass do
+        it "should be valid" do
+          something
+          1
+        end
+      end
+    }) }
+  end
   
 end
